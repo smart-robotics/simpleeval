bd715a5f00abc398c49fc6f8cb59cf83cf287fde
Daniel Claes <dclaes@smart-robotics.nl>
2021-09-08 13:01:49 +0200

    fix: remove 2to3

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     download_url='https://github.com/danthedeckie/simpleeval/tarball/' + version,
     keywords=['eval', 'simple', 'expression', 'parse', 'ast'],
     test_suite='test_simpleeval',
-    use_2to3=True,
     classifiers=['Development Status :: 4 - Beta',
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
